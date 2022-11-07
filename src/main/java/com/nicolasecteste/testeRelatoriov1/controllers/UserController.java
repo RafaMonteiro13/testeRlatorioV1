611c204f63244cba5aa9b217568391cf42178849
unknown <rafaelmonteirocaldas@gmail.com>
2022-11-07 09:00:25 -0300

    Adicionando dois metodos de http na classe controller,getById e Insert

--- src/main/java/com/nicolasecteste/testeRelatoriov1/controllers/UserController.java
+++ src/main/java/com/nicolasecteste/testeRelatoriov1/controllers/UserController.java
@@ -3,9 +3,7 @@ package com.nicolasecteste.testeRelatoriov1.controllers;
 import com.nicolasecteste.testeRelatoriov1.entities.User;
 import com.nicolasecteste.testeRelatoriov1.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +18,23 @@ public class UserController {
 
     @GetMapping
     //Informar qual o verbo http tu esta usando no caso GET
-    public List<User> findAll(){
-    List <User> result = repository.findAll();
-    return result;
+    public List<User> findAll() {
+        List<User> result = repository.findAll();
+        return result;
+    }
+
+    @GetMapping(value = "/{id}")
+    //pathVariable serve para referenciar o id do método http com o id variavel
+    public User findAById(@PathVariable Long id) {
+        //get serve oara pegar o usario de fato dentro desse obj
+        User result = repository.findById(id).get();
+        return result;
+    }
+
+    @PostMapping
+    //Usuario para salvar aparace no corpo da requisição post
+    public User insert(@RequestBody User user) {
+        User result = repository.save(user);
+        return result;
     }
 }
